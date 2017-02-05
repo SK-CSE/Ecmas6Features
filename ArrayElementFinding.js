ced798b45d05d9c74300a704400de67c32192952
SK-CSE <saurabh.cse04@gmail.com>
2017-02-05 15:50:24 +0530

    updated for POC

---
+++ ArrayElementFinding.js
@@ -0,0 +1,13 @@
+// ECMAScript 6
+// it will return first element greater than 3 or else return undefined
+var x =[ 1, 3, 4, 2, 5, 6 ].find(x => x > 3) // 4 
+console.log(x); //4
+
+
+// ECMAScript 5
+
+/*var x = [ 1, 3, 4, 2, 5, 6 ].filter(function (x) { return x > 3; })[0];
+console.log(x); //4
+
+var x1 = [ 1, 3, 4, 2, 5, 6 ].filter(function (x) { return x > 3; });
+console.log(x1); // [ 4, 5, 6 ]*/
